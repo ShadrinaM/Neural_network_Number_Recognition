691e94ee413e7e118ae67155cfb05e51fd7fd1c7
BusinaLapusina <busina_lapusina@mail.ru>
2024-10-10 23:57:18 +0300

    классы

---
+++ 35-1_Shadrina_Pricoldes_po_neiro/NeuroPricol/Network .cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _35_1_Shadrina_Pricoldes_po_neiro.NeuroPricol
+{
+    internal class Network
+    {
+    }
+}
