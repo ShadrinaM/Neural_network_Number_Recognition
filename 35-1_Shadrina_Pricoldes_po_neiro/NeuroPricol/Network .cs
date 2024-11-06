e14e7259e5c549ab71e8fc2719acb111aefd3ecd
BusinaLapusina <busina_lapusina@mail.ru>
2024-11-07 02:08:48 +0300

    циферки

--- 35-1_Shadrina_Pricoldes_po_neiro/NeuroPricol/Network .cs
+++ 35-1_Shadrina_Pricoldes_po_neiro/NeuroPricol/Network .cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace _35_1_Shadrina_Pricoldes_po_neiro.NeuroPricol
 {
+    //моя конфигурация 15 + 74 + 31 + 10
+    //входной слой(15 входных нейронов т.к. поле 3х5) + скрытый слой(74) + скрытый слой(31) + выходной слой(10 выходных нейронов т.к 10 цифр)   
     internal class Network
     {
     }
